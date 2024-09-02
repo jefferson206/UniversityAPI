a734f3c39bcf2add601f34d7aa895b298b1e0986
Jefferson Mendonca <jefferson.mendonca@theos.com.br>
2024-09-02 18:31:15 -0300

    Created Teachers controllers

---
+++ UniversityAPI/Controllers/TeachersController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversityAPI.Context;
+using UniversityAPI.Models;
+
+namespace UniversityAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TeachersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TeachersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Teacher>>> GetTeacher()
+        {
+            return await _context.Teacher.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Teacher>> GetTeacher(int id)
+        {
+            var teacher = await _context.Teacher.FindAsync(id);
+
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            return teacher;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTeacher(int id, Teacher teacher)
+        {
+            if (id != teacher.TeacherId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(teacher).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeacherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Teacher>> PostTeacher(Teacher teacher)
+        {
+            _context.Teacher.Add(teacher);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTeacher", new { id = teacher.TeacherId }, teacher);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTeacher(int id)
+        {
+            var teacher = await _context.Teacher.FindAsync(id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            _context.Teacher.Remove(teacher);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TeacherExists(int id)
+        {
+            return _context.Teacher.Any(e => e.TeacherId == id);
+        }
+    }
+}
